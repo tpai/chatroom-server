c2ee97d2215deed51f96c0f10540203f85109aad
Tony Pai <tony77794@gmail.com>
2018-12-13 11:23:42 +0800

    Add dotenv to handle environment variables

--- db.config.js
+++ db.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-  DB_HOST: 'localhost',
-  DB_NAME: 'db',
-  DB_USER: 'user',
-  DB_PASS: 'pass',
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
 };
